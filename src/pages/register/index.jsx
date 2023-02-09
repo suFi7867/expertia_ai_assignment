ab52bc180ed13a6f32888c05f3daecd67eb898bb
suFi7867 <noorani786.ss@gmail.com>
2023-02-10 00:19:41 +0530

    dashboard

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -102,6 +102,7 @@ export default function Register() {
             </div>
 
 
+
             <img className="bg-cover bg-center hidden lg:block w-[100%] scale-125 lg:w-[50%]" src='./image.svg' /> 
 
 
