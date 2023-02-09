4db2c728d5f94af8a7fbf3f4e823859f55c5045a
suFi7867 <noorani786.ss@gmail.com>
2023-02-10 00:36:00 +0530

    LOADING INDICATOR

---
+++ src/components/LoadingIndicator.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default LoadingIndicator = () => {
+  return (
+      <div className='relative flex w overflow-hidden text-left text-base text-black font-poppins justify-center max-w-[1200px] md-w-'>
+
+  
+     
+
+
+
+
+      </div>
+  )
+}
+
+
