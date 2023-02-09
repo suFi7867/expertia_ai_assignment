99df1e436ea8be23d13e99dfea6c9553008e11db
suFi7867 <noorani786.ss@gmail.com>
2023-02-09 23:15:11 +0530

    LOGIN PAGE DONE

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from "next/link";
 
 
 export default function Login() {
+
+    let initialValue = {
+        email : "" ,
+        password : ""
+    }
+    const [userData, setUserData] = useState(initialValue)
+
+    const handleChange = (e) => {
+
+        const { name: key, value, type, checked } = e.target;
+        const val = type === "checkbox" ? checked : value;
+        setUserData({ ...userData, [key]: val });
+
+    };
+
+    const LoginFunction = ()=> {
+        console.log(userData)
+        setUserData(initialValue)
+    }
+
+
+
   return (
-      <div className='flex w overflow-hidden text-left text-base text-black font-poppins justify-center'>
+      <div className='flex w overflow-hidden text-left text-base text-black font-poppins justify-center max-w-[1200px] md-w-'>
          
-          <div className="flex-1 border rounded-[10px] bg-white shadow-[0px_4px_64px_rgba(0,_0,_0,_0.05)] box-border h-[701px] max-w-[90%] sm:w-[100%] md:min-w-[505px] lg:min-w-[404px] xl:min-w-[505px] border-[0.5px] border-solid border-gray-500 p-5 sm:p-5 md:p-10 lg:p-10">
+          <div className="flex-1 border rounded-[10px] bg-white shadow-[0px_4px_64px_rgba(0,_0,_0,_0.05)] box-border h-[701px] max-w-[90%] sm:w-[100%] md:max-w-[505px] lg:max-w-[404px] xl:max-w-[505px] border-[0.5px] border-solid border-gray-500 p-5 sm:p-5 md:p-10 lg:p-10">
               <h1 className='text-[25px] font-light' >Welcome !</h1>
 
               <div className='flex flex-col mt-[50px] font-medium space-y-3' >
@@ -15,27 +37,40 @@ export default function Login() {
              </div>
 
 
-             <form className='mt-[50px]'>
+             <div className='mt-[50px]'>
                   <div className="mb-10">
                       <label className="block text-sm font-bold mb-2 text-[16px] font-normal" for="User Name">
                           User Name
                       </label>
-                      <input style={{border:"0.5px solid"}} className="rounded w-full py-4 px-5 text-gray-500 text-[14px] rounded-[6px] focus:outline-none" id="username" type="text" placeholder="Username"/>
+                      <input 
+                      onChange={handleChange}
+                      value={userData.email}
+                      name="email"
+                      style={{border:"0.5px solid"}} className="rounded w-full py-4 px-5 text-gray-500 text-[14px] rounded-[6px] focus:outline-none" id="username" type="email" placeholder="Username"/>
                   </div>
 
                   <div className="mb-4">
                       <label className="block text-sm font-bold mb-2 text-[16px] font-normal" for="User Name">
                           Password
                       </label>
-                      <input style={{border:"0.5px solid"}} className="rounded w-full py-4 px-5 text-gray-500 text-[14px] rounded-[6px] focus:outline-none" id="username" type="text" placeholder="Username" />
+                      <input
+                      onChange={handleChange}
+                      value={userData.password}
+                      name="password"
+                      style={{border:"0.5px solid"}} className="rounded w-full py-4 px-5 text-gray-500 text-[14px] rounded-[6px] focus:outline-none" id="username" type="text" placeholder="Username" />
                   </div>
 
 
                   <div className='flex justify-between mb-10'>
-                      <p className='text-[12px]' >Remember me</p>  <p className='text-[12px] text-[#4D4D4D]' >Forgot Password ?</p>
+                      
+                      <div className='flex justify-between gap-2'>
+                          <input type="checkbox" />  <p className='text-[12px]' > Remember me</p> 
+                      </div>
+                       <p className='text-[12px] text-[#4D4D4D]' >Forgot Password ?</p>
                   </div>
 
-                  <button className='mb-12 bg-black text-white w-[100%] p-5 rounded-[6px]'>
+                  <button onClick={LoginFunction}
+                  className='mb-12 bg-black text-white text-[20px] w-[100%] p-5 rounded-[6px]'>
                       Login
                   </button>
 
@@ -44,7 +79,7 @@ export default function Login() {
                       <Link href="/register"><p className='text-gray-900 font-bold'>Register</p></Link> 
                   </div>
 
-             </form>
+             </div>
              
          </div>
 
