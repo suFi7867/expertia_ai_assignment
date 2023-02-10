63bb64c7ca98d5fa1b345b67c0111f131401d2cc
suFi7867 <noorani786.ss@gmail.com>
2023-02-10 16:23:42 +0530

    L:OADING INDCATOR

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 
 export default LoadingIndicator = () => {
   return (
-      <div className='relative flex w overflow-hidden text-left text-base text-black font-poppins justify-center max-w-[1200px] md-w-'>
-
-  
-     
-
-
-
-
-      </div>
+    <div id="dashbord-div">
+      <img src='https://dltqhkoxgn1gx.cloudfront.net/img/posts/6-vue-loader-animation-libraries-to-reduce-your-bounce-rate-2.png' />
+    </div>
   )
 }
 
